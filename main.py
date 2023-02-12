153637e48c49d85af2e89bf94569ac5b2dcf7302
acatrin <catrinalexandra@yahoo.com>
2023-02-12 15:13:14 +0000

    Code updated to suggest wearing a jacket or not based on temperature.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import datetime
 from meteostat import Point, Daily
 
 # Set time period
-start = datetime.datetime.now() + datetime.timedelta(days=1)
-end = datetime.datetime.now() + datetime.timedelta(days=2)
+start = datetime.datetime.now() + datetime.timedelta(days=0)
+end = datetime.datetime.now() + datetime.timedelta(days=1)
 
 # Create Point for London, UK
 location = Point(51.5072, 0.1276)
@@ -15,3 +15,11 @@ data = data.fetch()
 
 # Display results to terminal
 print(data)
+
+temperature = data['tavg'][0]
+
+if temperature < 20:
+    print("You should wear a jacket.")
+else:
+    print("You don't need a jacket.")
+
